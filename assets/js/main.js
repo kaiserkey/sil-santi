b33c8b0b56efa746dd9da100ff8075542e166adb
Daniel <daniel.gonzalez@beclevercorp.com>
2024-07-30 12:05:59 -0300

    new changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ function updateCountdown() {
     const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
-
+    
     document.getElementById('days').textContent = days;
     document.getElementById('hours').textContent = hours;
     document.getElementById('minutes').textContent = minutes;
